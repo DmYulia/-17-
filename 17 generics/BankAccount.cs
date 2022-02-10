61ffd9152dc831e429a058692c48d5297ac2ecac
yulad <yulad@MATEBOOK>
2022-02-10 16:54:41 +0300

    Добавьте файлы проекта.

---
+++ 17 generics/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _17_generics
+{
+    internal class BankAccount<T>
+    {
+        public T Account { get; set; }
+        public string FullName { get; set; }
+        public double Score { get; set; }
+        public BankAccount()
+        {
+            WriteLine($"класс создан и заполнен конструктором без параметров.");
+        }
+        public BankAccount(T value, string name, double sc)
+        {
+            Account = value;
+            FullName = name;
+            Score = sc;
+            WriteLine($"класс создан и заполнен конструктором с параметрами.");
+        }
+        public void ShowInfo()
+        {
+            WriteLine($"User {FullName} bankaccount is {Account}.\nMoney: {Score}.");
+        }
+    }
+}
---
+++ 17 generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+namespace _17_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 457747753;
+            string s = "Фамилия Имя Отчество";
+            double m = 45.4878537;
+            BankAccount<int> intaccount = new BankAccount<int>(n, s, m);
+            string N = "SL46216647";
+            string S = "Family FirstName SecondName";
+            double M = -45612.121;
+            BankAccount<string> straccount = new BankAccount<string>(N, S, M);
+            intaccount.ShowInfo();
+            straccount.ShowInfo();
+            WriteLine();
+            BankAccount<int> intaccount2 = new BankAccount<int>();
+            intaccount2.Account = n;
+            intaccount2.FullName = s;
+            intaccount2.Score = m;
+            BankAccount<string> straccount2 = new BankAccount<string>();
+            straccount2.Account = N;
+            straccount2.FullName = S;
+            straccount2.Score = M;
+            intaccount2.ShowInfo();
+            straccount2.ShowInfo();
+            ReadLine();
+        }
+    }
+}
